87ce58d159c87cd0fe9acc7e80128ceab7822a6d
Aaron Hedges <aaron@dashron.com>
2019-05-14 15:12:41 -0400

    add example files for everything

---
+++ example/config/config.docker.example.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "web": {
+        "port": 80,
+        "layoutPath": "/code/example/templates/layout.hbs",
+        "pageNotFoundPath": "/code/example/templates/404.hbs",
+        "api": {
+            "hostname": "api",
+            "secure": false,
+            "port": 80
+        }
+    },
+    "api": {
+        "port": 80,
+        "PGSSL": "/code/example/config/postgres-cert.txt"
+    }
+};
